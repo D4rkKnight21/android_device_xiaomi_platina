5ef94181d40aa536cdcb3327eeb025a29b41d92a
D4rkKnight21 <yusuff8621@gmail.com>
2021-06-25 07:15:41 +0700

    platina: bringup RevengeOs

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_platina.mk
+    $(LOCAL_DIR)/revengeos_platina.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_platina-user \
-    lineage_platina-userdebug \
-    lineage_platina-eng
+    revengeos_platina-user \
+    revengeos_platina-userdebug \
+    revengeos_platina-eng
