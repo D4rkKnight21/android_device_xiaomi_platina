b217e4a10fef3c3c2785b472389e8dc9284060d2
D4rkKnight21 <yusuff8621@gmail.com>
2021-07-01 17:22:38 +0700

    platina: bringup dotOs Fan Edition

--- dotfe_platina.mk
+++ dotfe_platina.mk
@@ -19,8 +19,15 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 
-# Inherit some common Lineage stuff
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common dotOs Fan Edition stuff
+$(call inherit-product, vendor/dotfe/config/common.mk)
+
+# Bootanimation 
+TARGET_BOOT_ANIMATION_RES := 1080
+
+# dotOs Fan Edition stuff
+TARGET_FACE_UNLOCK_SUPPORTES := true
+TARGET_USES_BLUR := true
 
 # Inherit from platina device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
@@ -28,7 +35,7 @@ $(call inherit-product, $(LOCAL_PATH)/device.mk)
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := platina
 PRODUCT_MANUFACTURER := Xiaomi
-PRODUCT_NAME := lineage_platina
+PRODUCT_NAME := dotfe_platina
 PRODUCT_MODEL := MI 8 Lite
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
