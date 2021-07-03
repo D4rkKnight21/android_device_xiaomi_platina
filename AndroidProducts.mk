19258fa7265eeb54038d1f757d01b673579bc54c
D4rkKnight21 <yusuff8621@gmail.com>
2021-07-03 20:27:07 +0700

    platina: bringup cafex

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_platina.mk
+    $(LOCAL_DIR)/cafex_platina.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_platina-user \
-    lineage_platina-userdebug \
-    lineage_platina-eng
+    cafex_platina-user \
+    cafex_platina-userdebug \
+    cafex_platina-eng
